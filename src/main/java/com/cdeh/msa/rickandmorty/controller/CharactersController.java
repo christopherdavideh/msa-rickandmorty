afdaf5488809b04fbfbea073cdc0c8ec40d21254
christopherdavideh <chisk8_28@hotmail.com>
2024-04-10 01:07:45 -0500

    Add log4j

--- src/main/java/com/cdeh/msa/rickandmorty/controller/CharactersController.java
+++ src/main/java/com/cdeh/msa/rickandmorty/controller/CharactersController.java
@@ -2,24 +2,30 @@ package com.cdeh.msa.rickandmorty.controller;
 
 import com.cdeh.msa.rickandmorty.service.ICharactersService;
 import com.cdeh.msa.rickandmorty.service.dto.Character;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
 public class CharactersController {
+
+    private static final Logger logger = LogManager.getLogger(CharactersController.class);
     @Autowired
     ICharactersService charactersService;
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("characters")
     public List<Character> getCharacters(){
+        logger.info(charactersService.listCharacters());
         return charactersService.listCharacters();
     }
 }
--- src/main/java/com/cdeh/msa/rickandmorty/service/impl/CharactersServiceImpl.java
+++ src/main/java/com/cdeh/msa/rickandmorty/service/impl/CharactersServiceImpl.java
@@ -1,9 +1,12 @@
 package com.cdeh.msa.rickandmorty.service.impl;
 
+import com.cdeh.msa.rickandmorty.controller.CharactersController;
 import com.cdeh.msa.rickandmorty.repository.ICharactersRepository;
 import com.cdeh.msa.rickandmorty.service.ICharactersService;
 import com.cdeh.msa.rickandmorty.service.dto.Character;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +14,17 @@ import java.util.List;
 
 @Service
 public class CharactersServiceImpl implements ICharactersService {
+    private static final Logger logger = LogManager.getLogger(CharactersController.class);
     @Autowired
     ICharactersRepository characterRepository;
 
     @Override
     public List<Character> listCharacters() {
         try {
+            logger.info(characterRepository.getCharacter());
             return characterRepository.getCharacter();
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            logger.error("ServiceCDEH: " + e.getMessage());
             throw new RuntimeException("Ha occurido un error en la consulta");
         }
     }
